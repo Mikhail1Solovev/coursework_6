17f77e95acc038058ad4ea0c69f97e49e6fa9a76
Mihail Solovev <mikhail1solovev@gmail.com>
2024-08-29 17:52:35 +0500

    Initial commit

--- mailings/urls.py
+++ mailings/urls.py
@@ -6,24 +6,24 @@ from .views import (
 )
 
 urlpatterns = [
-    # Mailing URLs
+    # URLs for Mailing
     path('mailings/', MailingListView.as_view(), name='mailing_list'),
     path('mailings/create/', MailingCreateView.as_view(), name='mailing_create'),
-    path('mailings/<int:pk>/edit/', MailingUpdateView.as_view(), name='mailing_update'),
     path('mailings/<int:pk>/', MailingDetailView.as_view(), name='mailing_detail'),
+    path('mailings/<int:pk>/edit/', MailingUpdateView.as_view(), name='mailing_edit'),
     path('mailings/<int:pk>/delete/', MailingDeleteView.as_view(), name='mailing_delete'),
 
-    # Message URLs
+    # URLs for Message
     path('messages/', MessageListView.as_view(), name='message_list'),
     path('messages/create/', MessageCreateView.as_view(), name='message_create'),
-    path('messages/<int:pk>/edit/', MessageUpdateView.as_view(), name='message_update'),
     path('messages/<int:pk>/', MessageDetailView.as_view(), name='message_detail'),
+    path('messages/<int:pk>/edit/', MessageUpdateView.as_view(), name='message_edit'),
     path('messages/<int:pk>/delete/', MessageDeleteView.as_view(), name='message_delete'),
 
-    # Client URLs
+    # URLs for Client
     path('clients/', ClientListView.as_view(), name='client_list'),
     path('clients/create/', ClientCreateView.as_view(), name='client_create'),
-    path('clients/<int:pk>/edit/', ClientUpdateView.as_view(), name='client_update'),
     path('clients/<int:pk>/', ClientDetailView.as_view(), name='client_detail'),
+    path('clients/<int:pk>/edit/', ClientUpdateView.as_view(), name='client_edit'),
     path('clients/<int:pk>/delete/', ClientDeleteView.as_view(), name='client_delete'),
 ]
